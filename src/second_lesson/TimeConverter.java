279757932b9e22d614ddfc4f42e39df2a30d3970
artem <artempostol@gmail.com>
2020-11-26 18:32:05 +0200

    Home work 1

--- src/second_lesson/TimeConverter.java
+++ src/second_lesson/TimeConverter.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class TimeConverter {
     public static void main(String[] args) {
-        final int MINUTES_IN_HOUR = 60;
-        final int HOURS_IN_DAY = 24;
-        final int DAYS_IN_YEAR = 365;
+        final int MINUTES_IN_DAY = 24 * 60;
+        final int MINUTES_IN_YEAR = 60 * 24 * 365;
 
         System.out.print("Введите минуты: ");
         Scanner scan = new Scanner(System.in);
 
         int input = scan.nextInt();
-        int years = input/(MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_YEAR);
-        int days = (input%(MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_YEAR))/(MINUTES_IN_HOUR * HOURS_IN_DAY);
+        int years = input/(MINUTES_IN_YEAR);
+        int days = (input%(MINUTES_IN_YEAR))/(MINUTES_IN_DAY);
 
         System.out.println (input + " минут - это примерно " + years + " лет и " + days + " дней.");
     }
