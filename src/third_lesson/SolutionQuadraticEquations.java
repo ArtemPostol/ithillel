0e8d0e0b53581d778aa228ff102bccd547efcdb7
artem <artempostol@gmail.com>
2020-11-28 20:43:43 +0200

    Home work #3

--- src/third_lesson/SolutionQuadraticEquations.java
+++ src/third_lesson/SolutionQuadraticEquations.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class SolutionQuadraticEquations {
     public static void main(String[] args) {
         double a, b, c, x, x1, x2, d;
-        double exhibitor = 2;
-        int DISCRIMINANT_CALCULATING_CONST = 4;
-        int ROOT_CALCULATING_CONST = 2 ;
+        final double exhibitor = 2;
+        final int discriminantCalculationConstant = 4;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Эта программа решает квадратное уравнение вида: ax^2 + bx + c = 0");
@@ -18,14 +17,15 @@ public class SolutionQuadraticEquations {
         System.out.println("Введите c: ");
         c = input.nextDouble();
 
-        d = Math.pow(b, exhibitor) - (DISCRIMINANT_CALCULATING_CONST * a * c);
+        d = Math.pow(b, exhibitor) - (discriminantCalculationConstant * a * c);
+        double rootCalculatingVariable = 2 * a;
         if (d > 0) {
-            x1 = (-b + Math.sqrt(d)) / (ROOT_CALCULATING_CONST * a);
-            x2 = (-b - Math.sqrt(d)) / (ROOT_CALCULATING_CONST * a);
-            System.out.println("Корни уравнения равны: x1 = " + x1 + ", x2 = " + x2);
+            x1 = (-b + Math.sqrt(d)) / rootCalculatingVariable;
+            x2 = (-b - Math.sqrt(d)) / rootCalculatingVariable;
+            System.out.println("Корни уравнения равны: x1 =" + x1 + ", x2 =" + x2);
         } else if (d == 0) {
-            x = -b / (ROOT_CALCULATING_CONST * a);
-            System.out.println("Уравнение имеет единственный корень: x = " + x);
+            x = -b / rootCalculatingVariable;
+            System.out.println("Уравнение имеет единственный корень: x =" + x);
         } else {
             System.out.println("Уравнение не имеет  корней");
         }
