1badd94fdc60408aa812475e2c2daa4e66b0b6fe
artem <artempostol@gmail.com>
2020-11-29 20:49:07 +0200

    Home work #4 fix

--- src/fourth_lesson/SumAndMeanValue.java
+++ src/fourth_lesson/SumAndMeanValue.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SumAndMeanValue {
     public static void main(String[] args) {
         int a;
-        int value =0;
+        int value = 0;
         int digitCount = 5;
         Scanner input = new Scanner(System.in);
         for (int i = 1; i < 6; ++i) {
