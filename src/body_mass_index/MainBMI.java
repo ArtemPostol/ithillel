7daf7f131984d8f2474be43afdb534d94cc4e20f
artem <artempostol@gmail.com>
2020-12-06 20:22:39 +0200

    Home work #5 part 1

--- src/body_mass_index/MainBMI.java
+++ src/body_mass_index/MainBMI.java
@@ -7,11 +7,20 @@ public class MainBMI {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите ваше имя: ");
         String name = input.nextLine();
+
         System.out.println("Введите ваш вес в киллограмах: ");
         double weight = input.nextDouble();
+        if (weight <= 0 || weight >= 650) { //верхнее значение вес самого тяжелого человекка
+            System.out.println("Не бвает людей с весом " + weight + " кг.");
+            return;
+        }
+
         System.out.println("Введите ваш  рост в метрах: ");
         double height = input.nextDouble();
-
+        if (height <= 0 || height >= 2.72) { //верхнее значение рост самого высокого человекка
+            System.out.println("Не бвает людей с ростом " + height + " м.");
+            return;
+        }
 //        System.out.println(BodyMassIndex.calculateBodyMassIndex(weight, height));
         double bmi = BodyMassIndex.calculateBodyMassIndex(weight, height);
 
