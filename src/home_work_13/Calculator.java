26c17f8340b418817dbbb0cd8a8ef77a0dab8b2a
artem <artempostol@gmail.com>
2020-12-29 14:00:40 +0200

    Home work #13 part 1

---
+++ src/home_work_13/Calculator.java
@@ -0,0 +1,15 @@
+package home_work_13;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost(){
+        return floor.getArea() * carpet.getCost();
+    }
+ }
